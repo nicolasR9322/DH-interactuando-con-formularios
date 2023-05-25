9c2a8cffa1be0f891fc179d6c6f892ce1e2170ca
nicolasR9322 <nicolas.a.rodriguez93@gmail.com>
2023-05-25 18:23:00 -0300

    desafio 1

--- public/js/moviesAddValidation.js
+++ public/js/moviesAddValidation.js
@@ -2,6 +2,23 @@ window.onload = function(){
     let titulo = document.querySelector('.moviesAddTitulo')
     let formulario = document.querySelector('#formulario');
     let article = document.querySelector('article');
+    let titleInput = document.querySelector("#title");
+    let titleError = document.querySelector(".titleError");
+    let ratingInput = document.querySelector("#rating");
+    let ratingError = document.querySelector(".ratingError");
+    let awards = document.querySelector("#awards");
+    let awardsError = document.querySelector(".awardsError");
+    let release = document.querySelector("#release_date");
+    let releaseError = document.querySelector(".releaseError");
+    let length = document.querySelector("#length");
+    let lengthError = document.querySelector(".lengthError");
+    let genre = document.querySelector("#genre_id");
+    let genreError = document.querySelector(".genreError");
+    let form = document.querySelector("form");
+    let submitErrors = document.querySelector(".submitErrors");
+    
+    titleInput.focus()
+
     titulo.innerHTML = 'AGREGAR PELÍCULA';
     titulo.classList.add('titulo');
     article.classList.add('fondoTransparente');
@@ -10,6 +27,173 @@ window.onload = function(){
 //------DESDE AQUÍ CONTINÚE CON LAS VALIDACIONES DEL FORMULARIO //
 //-------------------DE REGISTRO DE PELÍCULAS------------------//    
 
+    // validacion nombre
+    let listener = ["keyup", "blur", "change"]
+    let listenerGenre =  ["click", "blur"]
+
+    listener.forEach((listen) => 
+    titleInput.addEventListener(listen, () => {
+            switch (true) {
+                case !titleInput.value.trim():
+                        console.log("vacio")
+                        titleError.classList.add("is-invalid")
+                        titleError.innerText = "Este campo no puede estar vacio"
+                    break;
+                default:
+                    console.log("lleno")
+                    titleError.classList.remove("is-invalid");
+                    title.classList.remove("is-invalid");
+                    titleInput.classList.add("is-valid");
+                    titleError.innerText = "";
+                    break;
+            }
+    })
+    )
+
+    
+    // validacion rating
+    listener.forEach((listen) => 
+    ratingInput.addEventListener(listen, () => {
+            switch (true) {
+                case !ratingInput.value.trim():
+                        console.log("vacio")
+                        ratingError.classList.add("is-invalid")
+                        ratingError.innerText = "Este campo no puede estar vacio"
+                    break;
+                case (ratingInput.value < 0 || ratingInput.value > 10):
+                        console.log("numero 1")
+                        console.log(ratingInput.value)
+                        ratingError.classList.add("is-invalid")
+                        ratingError.innerText = "Este campo debe contener un valor entre 0 y 10"
+                    break;
+                default:
+                    console.log("lleno")
+                    ratingError.classList.remove("is-invalid");
+                    rating.classList.remove("is-invalid");
+                    ratingInput.classList.add("is-valid");
+                    ratingError.innerText = "";
+                    break;
+            }
+    })
+    )
+
+    // validacion awards
+    listener.forEach((listen) => 
+    awards.addEventListener(listen, () => {
+            switch (true) {
+                case !awards.value.trim():
+                        console.log("vacio")
+                        awardsError.classList.add("is-invalid")
+                        awardsError.innerText = "Este campo no puede estar vacio"
+                    break;
+                case (awards.value < 0 || awards.value > 10):
+                        console.log("vacio")
+                        awardsError.classList.add("is-invalid")
+                        awardsError.innerText = "Este campo debe contener un valor entre 0 y 10"
+                    break;
+                default:
+                    console.log("lleno")
+                    awardsError.classList.remove("is-invalid");
+                    awards.classList.remove("is-invalid");
+                    awards.classList.add("is-valid");
+                    awardsError.innerText = "";
+                    break;
+            }
+    })
+    )
+
+    // validacion release_date
+    listener.forEach((listen) => 
+    release.addEventListener(listen, () => {
+            switch (true) {
+                case !release.value.trim():
+                        console.log("vacio")
+                        releaseError.classList.add("is-invalid")
+                        releaseError.innerText = "Este campo no puede estar vacio"
+                    break;
+                default:
+                    console.log("lleno")
+                    releaseError.classList.remove("is-invalid");
+                    release.classList.remove("is-invalid");
+                    release.classList.add("is-valid");
+                    releaseError.innerText = "";
+                    break;
+            }
+    })
+    )
+
+    // validacion length
+    listener.forEach((listen) => 
+    length.addEventListener(listen, () => {
+            switch (true) {
+                case !length.value.trim():
+                        console.log("vacio");
+                        lengthError.classList.add("is-invalid");
+                        lengthError.innerText = "Este campo no puede estar vacio"
+                    break;
+                case (length.value < 60 || length.value > 360):
+                        console.log("vacio");
+                        lengthError.classList.add("is-invalid");
+                        lengthError.innerText = "Este campo debe contener una valor entre 60 y 360"
+                    break;
+                default:
+                    console.log("lleno")
+                    lengthError.classList.remove("is-invalid");
+                    length.classList.remove("is-invalid");
+                    length.classList.add("is-valid");
+                    lengthError.innerText = "";
+                    break;
+            }
+    })
+    )
+
+    // validacion Genre
+    listenerGenre.forEach((listen) => 
+    genre.addEventListener(listen, () => {
+            switch (true) {
+                case genre.value.length < 1:
+                        console.log("vacio")
+                        genreError.classList.add("is-invalid")
+                        genreError.innerText = "Este campo no puede estar vacio"
+                    break;
+                default:
+                    console.log("lleno")
+                    genreError.classList.remove("is-invalid");
+                    genre.classList.remove("is-invalid");
+                    genre.classList.add("is-valid");
+                    genreError.innerText = "";
+                    break;
+            }
+    })
+    )
+
+    form.addEventListener("submit", (e) => {
+
+        e.preventDefault()
+        console.log(titleInput)
+        const FORM_ELEMENTS = e.target.elements;
+
+        for (let i = 0; i < FORM_ELEMENTS.length -1; i++) {
+            const element = FORM_ELEMENTS[i];
+            if(element.value === ""){
+                let divErrores = element.parentElement.nextElementSibling;
+                divErrores.classList.add("is-invalid");
+                divErrores.innerHTML = `<li>no cumple con los requisitos</li>` 
+            }
+        }
+
+        let elementosConErrores = document.querySelectorAll(".is-invalid");
+        let errores = elementosConErrores.length > 0;
+
+        console.log(errores)
+        console.log(elementosConErrores)
+        if(errores){
+            submitErrors.style.color = "red"
+            submitErrors.innerHTML = "por favor, revise los errores en su formulario"
+        } else {
+            form.submit()
+        }
+    })
 
 
 
--- readme.md
+++ readme.md
@@ -4,4 +4,22 @@ descripcion: El objetivo será asegurarnos que se completene todos los controles
 
     base :
         -proyecto proporcionado
+
+    desafio 1 :
+        -debemos vincular el archivo moviesAddValidation.js y vincularlo con la vista moviesAdd.ejs
+
+        - todos los campos son obligatorios, no pueden estar vacios
+
+        - si se trata de enviar un formulario sin los campos necesarios, se debe negar el envio del formulario y agregarle la clase "is-invalid" a los campos que no cumplan los requisitos
+
+        -las validaciones deben hacerse al momento de enviar el formulario y tambien en tiempo real
+
+        -aquellos campos que poseen error deberan tener un texto que especifique el error, deben estar presentes en el elemento con clase "is-invalid"
         
+        -el formulario contara con validaciones especiales para los siguientes campos
+            -calificacion y premios : valor ingresado entre 0 y 10
+            -duracion: entre 60 y 360 minutos
+        
+        -si los campos cumplen con los requisitos:
+            -a todos los campos se le agrega la clase "is-valid"
+    
